7c1e5e2158b06e7f4142e19c71c532d49ea69cef
Nick Cao <nickcao@nichi.co>
2022-10-16 23:03:30 +0800

    use the correct call package

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs.nixpkgs.url = "github:NickCao/nixpkgs/nixos-unstable-small";
   outputs = { self, nixpkgs, ... }: with nixpkgs.legacyPackages.x86_64-linux; {
-    packages.x86_64-linux.dhack = callPackage ./dhack.nix { inherit (linuxPackages_latest) kernel; };
+    packages.x86_64-linux.dhack = linuxPackages_latest.callPackage ./dhack.nix { };
   };
 }
